64e427b7e83ee55d72f48734dd3e18ba0d7e2f7f
AdityaThapa1 <102941877+AdityaThapa1@users.noreply.github.com>
2022-06-23 10:55:37 +0545

    Create arrangedescending.c

---
+++ arrangedescending.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main(){
+	int arr[8]={4,8,7,9,15,10,6,1};
+	int i,j,k,m;
+
+	int num,temp;
+	
+	for(j=0;j<7;j++){
+		temp=arr[j];
+	
+		
+		for(i=(j+1);i<8;i++){
+			if (temp<arr[i]){
+			
+			temp=arr[i];
+			arr[i]=arr[j];
+		
+			arr[j]=temp;	
+		
+			
+			}
+		}
+	
+		
+	}
+	printf("\n the sorted array in descendeing order is: \n");
+	for(m=0;m<8;m++){
+		printf("%d \t", arr[m]);
+			
+	}
+		
+	
+	return 0;
+	
+}
